33898be325cdf8fccf1784436f25fd7a0911d222
valderezpe <valderezpe@gmail.com>
2021-09-06 22:01:58 -0300

    Project created

---
+++ backend/src/main/java/com/meuprojeto/controlevendas/ControlevendasApplication.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.controlevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlevendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/meuprojeto/controlevendas/ControlevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.controlevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
