6ddfc06b1581715c3da79e97450b7a16df8561d7
valderezpe <valderezpe@gmail.com>
2021-09-07 10:32:41 -0300

    Basic static components

---
+++ fontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+
+const  Footer=() => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/valderezpe" target="_blank" rel="noreferrer">Valderez</a></p>
+          <p className="text-light"><small><strong>Project Spring React</strong><br/>
+            Objetivo e Mostrar como podemos usar Spring, React: <a href="" target="_blank" rel="noreferrer">valderezpe@gmail.com</a></small></p>
+        </div>
+      </footer>
+      
+      
+    );
+  }
+  
+  export default Footer;
+  
